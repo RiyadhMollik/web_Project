d1d74cbd41714174df0cf6707233f212ab3c43af
dipro <dipro@dipros-MacBook-Air.local>
2025-07-27 20:01:52 +0600

    added daisyui and tailwindcss

--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
-const NavBar = () => {
-    return (
-        <div className='center text-3xl font-bold text-blue-500'> 
-            helko from NavBar
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+          <Link to="/">MyApp</Link>
         </div>
-    );
+        <div className="space-x-4">
+          <Link to="/" className="text-white hover:text-gray-300">
+            Home
+          </Link>
+          <Link to="/footer" className="text-white hover:text-gray-300">
+            Footer
+          </Link>
+          <Link to="/navbar" className="text-white hover:text-gray-300">
+            NavBar
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default NavBar;  
+export default Navbar;
